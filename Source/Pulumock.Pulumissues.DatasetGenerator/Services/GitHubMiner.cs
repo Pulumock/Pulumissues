8d448e9c83e9e4c811c6018b524ce2682adb5df9
Piran <robertopiranamedi@gmail.com>
2025-04-18 16:17:41 +0200

    Add filtering on terms

--- Source/Pulumock.Pulumissues.DatasetGenerator/Services/GitHubMiner.cs
+++ Source/Pulumock.Pulumissues.DatasetGenerator/Services/GitHubMiner.cs
@@ -9,7 +9,7 @@ internal sealed class GitHubMiner<T>(IGitHubClient gitHubClient, IOptions<T> opt
 {
     public async Task<GitHubMinerResult> GetGitHubIssuesAsync()
     {
-        var allIssues = new List<GetGitHubIssuesAsyncResponse>();
+        var finalIssues = new List<GetGitHubIssuesAsyncResponse>();
         int page = 1;
         int pageLimit = 500;
         const int pageSize = 100;
@@ -18,11 +18,16 @@ internal sealed class GitHubMiner<T>(IGitHubClient gitHubClient, IOptions<T> opt
         {
             IReadOnlyCollection<GetGitHubIssuesAsyncResponse> currentPage = await gitHubClient.GetGitHubIssuesAsync(options, page, pageSize);
             
-            var onlyIssues = currentPage
-                .Where(i => i.PullRequest is null)
-                .ToList();
-
-            allIssues.AddRange(onlyIssues);
+            IEnumerable<GetGitHubIssuesAsyncResponse> filteredIssues = currentPage
+                .Where(i => i.PullRequest is null);
+            
+            if (options.Value.Terms is not null && options.Value.Terms.Count > 0)
+            {
+                filteredIssues = filteredIssues.Where(i => 
+                    ContainsTerm(i.Title, options.Value.Terms) || ContainsTerm(i.Body, options.Value.Terms));
+            }
+            
+            finalIssues.AddRange(filteredIssues);
 
             if (currentPage.Count < pageSize)
             {
@@ -35,7 +40,10 @@ internal sealed class GitHubMiner<T>(IGitHubClient gitHubClient, IOptions<T> opt
         return new GitHubMinerResult
         {
             Repository = options.Value.Repo,
-            Issues = allIssues.AsReadOnly()
+            Issues = finalIssues.AsReadOnly()
         };
     }
+
+    private static bool ContainsTerm(string text, List<string> terms) => 
+        terms.Any(t => text.Contains(t, StringComparison.InvariantCultureIgnoreCase));
 }
